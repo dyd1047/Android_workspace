99f362da1211e32115d58e4d4010802931e4bbae
YONG\msi <dyd414646@gmail.com>
2021-01-28 17:49:19 +0900

    January 28, 2021

---
+++ ActionBarApp/app/src/main/java/com/koreait/actionbarapp/gallery/GalleryAdapter.java
@@ -0,0 +1,29 @@
+package com.koreait.actionbarapp.gallery;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class GalleryAdapter extends BaseAdapter {
+    //리소스에 있는 자원 이용.. 네트워크 http 요청?(안정적) socket 요청?(미친놈)
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
